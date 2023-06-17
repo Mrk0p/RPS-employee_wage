4e1f2cb6828ec8cdd79aa2ddf706847035fb5ab3
Mrk0p <sachinwele23@gamil.com>
2023-06-17 20:31:04 +0530

    Added:employee monthly wage logic.

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -10,31 +10,39 @@ public class EmployeeWage {
         int fullDayHours = 8;
         int halfDayHours = 4;
         int dailyWage = 0;
+        int monthlyWage = 0;
+        int workingDaysPerMonth = 20;
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
         Random random = new Random();
 
-        int randomNum = random.nextInt(3);
+        for (int i = 1; i <= workingDaysPerMonth; i++) {
+            int randomNum = random.nextInt(3);
 
-        switch (randomNum) {
+            switch (randomNum) {
 
-            case 0:
-                System.out.println("Employee is Absent");
-                System.out.println("Monthly Salary will be: ");
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                System.out.println("Monthly salary for Full Day: ");
-                dailyWage = wagePerHour * fullDayHours;
-                break;
-            case 2:
-                System.out.println("Employee is Present");
-                System.out.println("Monthly Salary for Half Day: ");
-                dailyWage = wagePerHour * halfDayHours;
-                break;
+                case 0:
+                    System.out.println("Employee is Absent");
+                    System.out.println("Monthly wage will be: ");
+                    break;
+                case 1:
+                    System.out.println("Employee is Present");
+                    System.out.println("Monthly wage for Full Day: ");
+                    dailyWage = wagePerHour * fullDayHours;
+                    break;
+                case 2:
+                    System.out.println("Employee is Present");
+                    System.out.println("Monthly wage for Half Day: ");
+                    dailyWage = wagePerHour * halfDayHours;
+                    break;
+
+            }
+            monthlyWage = monthlyWage + dailyWage;
+            System.out.println(+dailyWage);
 
         }
-        System.out.println(+dailyWage);
+        System.out.println("Monthly Salary: " + monthlyWage);
+
     }
 }
