fc628e20771715b053ab06a3698751eab5074396
Mrk0p <sachinwele23@gamil.com>
2023-07-01 15:54:16 +0530

    Added:manage employee wage of multiple company using list

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -1,23 +1,22 @@
 package com.bridgelabz.employee_wage;
 
+import java.util.ArrayList;
+
 public class EmployeeWage implements IEmployeeWage {
 
     public static final int PART_TIME = 1;
     public static final int FULL_TIME = 2;
-    // instance variables
-    int noOfCompanies, index;
-    CompanyEmpWage[] companies;
+    ArrayList<CompanyEmpWage> companies;
 
-    public EmployeeWage(int noOfCompanies)
+    public EmployeeWage()
     {
-        this.noOfCompanies = noOfCompanies;
-        companies = new CompanyEmpWage[noOfCompanies];
-        index = 0;
+        companies = new ArrayList<>();
     }
 
     public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
     {
-        companies[index++] = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
+        CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
+        companies.add(company);
     }
 
     int generateEmployeeType()
@@ -69,10 +68,11 @@ public class EmployeeWage implements IEmployeeWage {
 
     public static void main(String args[])
     {
-        EmployeeWage employeeWage = new EmployeeWage(3);
+        EmployeeWage employeeWage = new EmployeeWage();
         employeeWage.addCompany("Microsoft", 4, 30, 100);
         employeeWage.addCompany("Google", 5, 40, 170);
         employeeWage.addCompany("Apple", 9, 10, 70);
+        employeeWage.addCompany("Amazon", 19, 10, 150);
         employeeWage.calculateTotalWage();
     }
 }
--- src/com/bridgelabz/employee_wage/IEmployeeWage.java
+++ src/com/bridgelabz/employee_wage/IEmployeeWage.java
@@ -4,4 +4,5 @@ public interface IEmployeeWage {
     public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
 
     public void calculateTotalWage();
+
 }
